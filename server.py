48e3c12f53428a2f916e66d496695d31c4497715
soalko <129190098+soalko@users.noreply.github.com>
2023-04-27 23:53:26 +0400

    Update server.py

--- server.py
+++ server.py
@@ -220,5 +220,6 @@ def bad_request(_):
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
